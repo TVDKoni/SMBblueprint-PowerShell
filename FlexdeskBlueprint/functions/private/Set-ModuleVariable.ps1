d6e98bb0bb9e376e1576cba29f1c09ccbcd36b7a
Konrad Brunner <konrad.brunner@trivadis.com>
2017-03-14 09:32:25 +0100

    Issue with Set-ModuleVariable on older servers

--- FlexdeskBlueprint/functions/private/Set-ModuleVariable.ps1
+++ FlexdeskBlueprint/functions/private/Set-ModuleVariable.ps1
@@ -1,7 +1,7 @@
 Function Set-ModuleVariable {
     [cmdletbinding()]
     param()
-    $Config = Get-Content "$Root\config.json"|ConvertFrom-Json
+    $Config = Get-Content "$Root\config.json" | Out-String | ConvertFrom-Json
     foreach($Property in ($Config.config|gm -MemberType NoteProperty).Name){
         $null = New-Variable -Scope Script -Name $Property -Value $($Config.config.$Property) -Force
         write-verbose "Creating property '$Property' with value '$($Config.config.$Property)'"
--- StartGUI.ps1
+++ StartGUI.ps1
@@ -1,3 +1,3 @@
 Import-Module Microsoft.Online.SharePoint.PowerShell
 Import-Module -name "$PSScriptRoot\FlexdeskBlueprint" -Verbose
-Start-FlexdeskDeploymentGUI -NoUpdateCheck
+Start-FlexdeskDeploymentGUI
